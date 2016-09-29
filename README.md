145eb55e188757b101752f6e540a648ce07f3be7
Dan Wu <zjlxwudan@gmail.com>
2016-09-29 00:39:29 -0500

    able to get json from moviedb

---
+++ README.md
@@ -0,0 +1,5 @@
+PopularMovies
+=============
+How To Compile
+--------------
+To successfully compile this project using Gradle, replace the {your_api_key} in gradle.properties with your own MovieDB api key.
--- app/src/main/java/com/example/zjlxw/popularmovies/ShowcaseFragment.java
+++ app/src/main/java/com/example/zjlxw/popularmovies/ShowcaseFragment.java
@@ -1,9 +1,12 @@
 package com.example.zjlxw.popularmovies;
 
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,7 +17,20 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ShowcaseFragment extends Fragment {
+
+    private final String LOG_TAG = ShowcaseFragment.class.getSimpleName();
+
     public ShowcaseFragment() {
     }
 
@@ -31,5 +47,95 @@ public class ShowcaseFragment extends Fragment {
             }
         });
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovies();
+    }
+
+    private void updateMovies() {
+        FetchMovieTask movieTask = new FetchMovieTask();
+        movieTask.execute();
+        Log.d(LOG_TAG, "updateMovies: execute FetchMovieTask");
+    }
+
+    private String[] getMovieDataFromJson(String movieJsonStr)
+        throws JSONException {
+        return null;
+    }
+
+    public class FetchMovieTask extends AsyncTask<String, Void, String[]> {
+        private final String LOG_TAG = FetchMovieTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String movieJsonStr = null;
+
+            try {
+                final String MOVIE_BASE_URL = "http://api.themoviedb.org/3/movie/popular?";
+                final String API_KEY = "api_key";
+                Uri builtUri = Uri.parse(MOVIE_BASE_URL).buildUpon()
+                        .appendQueryParameter(API_KEY, BuildConfig.MOVIEDB_API_KEY)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+                Log.d(LOG_TAG, "JSON: \n"+buffer);
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+
+                movieJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "IO Error: ", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream: ", e);
+                    }
+                }
+            }
+
+            try {
+                return getMovieDataFromJson(movieJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
