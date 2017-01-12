54eb67863710d77f09780167a2ea352396af7a62
Dan Wu <zjlxwudan@gmail.com>
2017-01-12 00:36:49 -0600

    can watch trailer now

--- app/src/main/java/com/example/zjlxw/popularmovies/DetailFragment.java
+++ app/src/main/java/com/example/zjlxw/popularmovies/DetailFragment.java
@@ -1,6 +1,12 @@
 package com.example.zjlxw.popularmovies;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +19,18 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -65,6 +83,105 @@ public class DetailFragment extends Fragment implements View.OnClickListener {
 
     public void onTrailerClicked(View view) {
         Log.d(LOG_TAG, "onTrailorClicked: " + mId);
+        FetchTrailerTask fetchTrailerTask = new FetchTrailerTask();
+        fetchTrailerTask.execute();
+    }
+
+    public boolean isOnline() {
+        Activity context = getActivity();
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
+
+    public class FetchTrailerTask extends AsyncTask<String, Void, String> {
+        private final String LOG_TAG = FetchTrailerTask.class.getSimpleName();
+
+        private String getTrailerFromJson(String movieJsonStr)
+                throws JSONException {
+            JSONObject movieJson = new JSONObject(movieJsonStr);
+            JSONArray movieArray = movieJson.getJSONArray("results");
+            JSONObject movieObject = movieArray.getJSONObject(0);
+            String key = movieObject.getString("key");
+            Log.d(LOG_TAG, "getTrailerFromJson: get trailer key" + key);
+            return key;
+        }
+
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String movieJsonStr = null;
+            if (isOnline()) {
+                try {
+                    String MOVIE_BASE_URL = "http://api.themoviedb.org/3/movie/" + mId + "/videos";
+                    final String API_KEY = "api_key";
+                    Uri builtUri = Uri.parse(MOVIE_BASE_URL).buildUpon()
+                            .appendQueryParameter(API_KEY, BuildConfig.MOVIEDB_API_KEY)
+                            .build();
+
+                    URL url = new URL(builtUri.toString());
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.connect();
+
+                    InputStream inputStream = urlConnection.getInputStream();
+                    StringBuilder buffer = new StringBuilder();
+                    if (inputStream == null) {
+                        return null;
+                    }
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                        buffer.append("\n");
+                    }
+                    Log.d(LOG_TAG, "JSON: \n" + buffer);
+
+                    if (buffer.length() == 0) {
+                        return null;
+                    }
+
+                    movieJsonStr = buffer.toString();
+
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "IO Error: ", e);
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (final IOException e) {
+                            Log.e(LOG_TAG, "Error closing stream: ", e);
+                        }
+                    }
+                }
+
+                try {
+                    return getTrailerFromJson(movieJsonStr);
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                Log.d(LOG_TAG, "onPostExecute: start trailer");
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=" + result)));
+            }
+        }
     }
 
     @Override
