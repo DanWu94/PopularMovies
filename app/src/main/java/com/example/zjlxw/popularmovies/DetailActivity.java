43497ad2f70b18e1dbb09c6b53b3d506c440168a
Dan Wu <zjlxwudan@gmail.com>
2017-03-04 23:42:13 -0600

    apply collapsing toolbar layout on detail page

--- app/src/main/java/com/example/zjlxw/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/zjlxw/popularmovies/DetailActivity.java
@@ -1,15 +1,12 @@
 package com.example.zjlxw.popularmovies;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.NavUtils;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -17,25 +14,36 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        if (savedInstanceState == null) {
-            Bundle extras = getIntent().getExtras();
-            if (extras != null) {
-                if (extras.containsKey("movie")) {
-                    Movie movie = extras.getParcelable("movie");
 
-                    Bundle arguments = new Bundle();
-                    arguments.putParcelable(DetailFragment.DETAIL_URI, movie);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            if (extras.containsKey("movie")) {
+                Movie movie = extras.getParcelable("movie");
 
-                    DetailFragment fragment = new DetailFragment();
-                    fragment.setArguments(arguments);
+                CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+                collapsingToolbarLayout.setTitle(movie.getTitle());
 
-                    getSupportFragmentManager().beginTransaction()
-                            .add(R.id.container, fragment)
-                            .commit();
-                }
+                ImageView imageView = (ImageView) findViewById(R.id.image);
+
+                Picasso.with(this)
+                        .load(movie.getImageUrl())
+                        .placeholder(R.drawable.placeholder)
+                        .error(R.drawable.placeholder_error)
+                        .into(imageView);
+
+                Bundle arguments = new Bundle();
+                arguments.putParcelable(DetailFragment.DETAIL_URI, movie);
+
+                DetailFragment fragment = new DetailFragment();
+                fragment.setArguments(arguments);
+
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.container, fragment)
+                        .commit();
             }
         }
     }
